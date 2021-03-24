79896931f280f8666b479e71b0e5d51628828fd1
Sébastien Delafond <sdelafond@gmail.com>
2021-03-24 14:08:22 +0100

    wrapper: initial version

---
+++ bin/ats-wrapper.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+set -eE
+
+## constants (FIXME: common.sh)
+BIN_DIR=$(dirname $(readlink -f $0))
+REPORTS_HOST="package-server.untangle.int"
+REPORTS_USER="buildbot"
+REPORTS_BASEDIR="/var/www/ats-on-demand"
+TS=$(date +"%Y%m%dt%H%M")
+TS_ISO=${TS/t/T}
+
+## main
+
+# CLI parameters
+if [ $# != 2 ] ; then
+  echo "Usage: $0 <version> <distribution>"
+  exit 1
+fi
+
+VERSION=$1
+DISTRIBUTION=$2
+
+IMAGE=ngfw:${VERSION}-${TS}
+ALLURE_LOCAL_VOLUME=./allure/${VERSION}/$TS_ISO
+
+${BIN_DIR}/ats-build-containers.sh $DISTRIBUTION $IMAGE
+${BIN_DIR}/ats-start-containers.sh $IMAGE
+${BIN_DIR}/ats-run-tests.sh $IMAGE
+${BIN_DIR}/ats-stop-containers.sh $IMAGE
+
+scp -i ~${USER}/.ssh/id_rsa -r $ALLURE_LOCAL_VOLUME ${REPORTS_USER}@${REPORTS_HOST}:${REPORTS_BASEDIR}/${VERSION}/
